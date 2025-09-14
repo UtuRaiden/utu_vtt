e408c539e8c219394c4203c655cedd27a34dd348
unknown <barronpeterson56@gmail.com>
2025-09-14 13:50:07 -0500

    add live inputs for the col and rows

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,24 +6,37 @@ function isodd(number) {
 
 const svg = document.getElementById("board");
 const radius = 50;
-const cols = 10;
-const rows = 10;
 
-const svgWidth = cols * Math.sqrt(3) * radius + radius * 2;
-const svgHeight = rows * 1.5 * radius + radius * 2;
-svg.setAttribute("width", svgWidth);
-svg.setAttribute("height", svgHeight);
+document.getElementById("gridcol").addEventListener("input", generatepointyhexgrid);
+document.getElementById("gridrow").addEventListener("input", generatepointyhexgrid);
 
-for (let col = 0; col < cols; col++) {
-    for (let row = 0; row < rows; row++) {
-        let x = col * (Math.sqrt(3) * radius) + radius;
-        let y = row * (1.5 * radius) + radius;    
-        console.log(row)
-        console.log(isodd(row))
 
-        if (isodd(row)){
-            x +=.87*radius;
-        }
-        hexagon(svg, x, y, radius);
+//generates a pointy top hex grid
+function generatepointyhexgrid(){
+
+    const cols = parseInt(document.getElementById("gridcol").value, 10) || 0;
+    const rows = parseInt(document.getElementById("gridrow").value, 10) || 0;
+
+      while (svg.firstChild) {
+        svg.removeChild(svg.firstChild);
     }
+
+    const svgWidth = cols * Math.sqrt(3) * radius + radius * 2;
+    const svgHeight = rows * 1.5 * radius + radius * 2;
+    svg.setAttribute("width", svgWidth);
+    svg.setAttribute("height", svgHeight);
+
+    for (let col = 0; col < cols; col++) {
+        for (let row = 0; row < rows; row++) {
+            let x = col * (Math.sqrt(3) * radius) + radius;
+            let y = row * (1.5 * radius) + radius;    
+            console.log(row)
+            console.log(isodd(row))
+
+            if (isodd(row)){
+                x +=.87*radius;
+        }
+            hexagon(svg, x, y, radius);
+    }
+}
 }
