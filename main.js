68221f97117e4ff82b75c6420691d99ef4710e2e
unknown <barronpeterson56@gmail.com>
2025-09-15 13:23:03 -0500

    add packaging software and custom icons

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { app, BrowserWindow } = require('electron/main')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: './assets/icons/utulogo',
   })
 
   win.loadFile('index.html')
